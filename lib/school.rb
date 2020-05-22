9ca853f815aba014fb5257ff019e1eb5d970074e
Michael Nnaji <mnnaji3@uwo.ca>
2020-05-22 00:20:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+attr_accessor :roster
+
+def initialize(school)
+  @school = school
+  @roster=  Hash.new {|h,k| h[k]=[]}
+end
+
+def add_student(student,grade)
+
+  @roster[grade]<< student
+
+
+end
+
+def grade(grades)
+  @roster[grades]
+end
+
+def sort
+  @roster.each_value {|value| value.sort!}
+
+end
+
+
+
+end
